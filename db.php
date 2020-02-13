a51a950964ed37ac6aca2c6700d81a512d5c8c27
Brage <brage.skjonborg@hotmail.no>
2020-02-13 21:57:22 +0100

    Updated db with charset

--- db.php
+++ db.php
@@ -38,6 +38,7 @@
         case "mysql":
         {
             $db = new mysqli($address, $username, $password, $database); //mysqli_connect($address, $username, $password, $database);
+            $db->set_charset("utf8");
             break;
         }
 
@@ -56,65 +57,5 @@
         echo json_encode($e);
         die();
     }
-
-
-    /**
-     * Variable to hold query type before defining
-     */
-    $query;
-
-    /**
-     * Variable to hold fetch type before defining
-     */
-    $fetch_array;
-
-    /**
-     * Variable to hold fetch type before defining
-     */
-    $fetch_object;
-
-    /**
-     * Variable to hold fetch type before defining
-     */
-    $fetch_assoc;
-
-
-    $num_rows;
-
-    /**
-     * Variable to hold close
-     */
-    $close;
-
-    switch($type)
-    {
-        /**
-         * If type is MySql
-         */
-        case "mysql":
-        {
-            $query            = 'mysqli_query';
-            $fetch_array      = 'mysqli_fetch_array';
-            $fetch_object     = 'mysqli_fetch_object';
-            $fetch_assoc      = 'mysqli_fetch_assoc';
-            $close            = 'mysqli_close';
-            $num_rows         = 'mysqli_num_rows';
-            break;
-        }
-
-
-    }
-
-/**
- * Defines constants
- */
-define('__type',            $type);
-define('__query',           $query);
-define('__fetch_array',     $fetch_array);
-define('__fetch_object',    $fetch_object);
-define('__fetch_assoc',     $fetch_assoc);
-define('__num_rows',        $num_rows);
-define('__close',           $close);
-
 
 ?>
